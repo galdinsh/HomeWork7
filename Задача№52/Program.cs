0eafd3375718b4c7be788204cbfa1ae76f4c42ea
Vladislav <galdinsh77@mail.ru>
2023-03-30 19:30:38 +0500

    Задача№52

---
+++ Задача№52/Program.cs
@@ -0,0 +1,37 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+void SumSredArifm(int[,] matrix)
+{
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            double sum = 0; int s = matrix.GetLength(0);
+            for(int i = 0; i < matrix.GetLength(0); i++) 
+                    sum = sum + matrix[i, j];
+        Console.Write($" {Math.Round((sum / s), 2)} ");
+        }
+}
+Console.Clear();
+Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.Write("Среднне арифметическое столбцов = ");
+SumSredArifm(matrix);
